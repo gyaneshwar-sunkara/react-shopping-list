ec848409888c1191d320a3b707939eede4666ed4
Gyaneshwar Sunkara <gyanisunkara1@gmail.com>
2020-08-09 13:59:18 +0530

    Final Commit

--- server.js
+++ server.js
@@ -1,25 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-
-const items = require('./routes/api/items');
 
 const app = express();
 
-// Bodyparser Middleware
-app.use(bodyParser.json());
-
-// DB Config
-const db = require('./config/key').mongoURI;
-
-// Connet to MongoDB
-mongoose
-    .connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => console.log('[MongoDB] Connected'))
-    .catch(err => console.log('[MongoDB]', err));
-
-// Use Routes
-app.use('/api/items', items);
 
 /**
  * Static Assets
